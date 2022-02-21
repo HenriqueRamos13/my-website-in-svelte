db877f745686c1b842ef57742f46c0c3262461b3
Henrique Ramos <henriqueramos@Ft3h-Ria.Home>
2022-02-21 07:56:24 +0000

    effects ok

---
+++ src/App.svelte
@@ -0,0 +1,303 @@
+<script lang="ts">
+  import Contact from "./components/Contact.svelte";
+  import Header from "./components/Header.svelte";
+  import Section from "./components/Section.svelte";
+
+  const STEPS = [
+    {
+      step: 1,
+      stepTitle: "Bem vindo!",
+      src: "images/image1.webp",
+      alt: "imagem",
+      title: "Henrique Ramos, Web Developer",
+      index: 0,
+    },
+    {
+      step: 2,
+      stepTitle: "Quem sou eu?",
+      src: "images/image1.webp",
+      alt: "imagem",
+      title: "Teste do Web Developer",
+      index: 1,
+    },
+    {
+      step: 3,
+      stepTitle: "Trabalhos",
+      src: "images/image1.webp",
+      alt: "imagem",
+      title: "Uma nova seção",
+      index: 2,
+    },
+    {
+      step: 3,
+      stepTitle: "Trabalhos",
+      src: "images/image1.webp",
+      alt: "imagem",
+      title: "Penultima seção",
+      index: 3,
+    },
+    {
+      step: 3,
+      stepTitle: "Trabalhos",
+      src: "images/image1.webp",
+      alt: "imagem",
+      title: "Ultima seção",
+      index: 4,
+    },
+    {
+      step: 4,
+      stepTitle: "Contato",
+      src: "images/image1.webp",
+      alt: "imagem",
+      title: "Contato",
+      index: 5,
+      component: Contact,
+    },
+  ];
+
+  let currentStep: number = 0;
+
+  type Step = {
+    step: number;
+    stepTitle: string;
+    src: string;
+    alt: string;
+    title: string;
+    index: number;
+    component?: any;
+  };
+
+  function debounce(func: Function, wait: number, immediate: boolean = false) {
+    let timeout;
+
+    return function (...t) {
+      const context = this,
+        d = immediate && !timeout;
+      clearTimeout(timeout),
+        (timeout = setTimeout(function () {
+          (timeout = null), immediate || func.apply(context, t);
+        }, wait)),
+        d && func.apply(context, t);
+    };
+  }
+
+  function moveSections(direction: number) {
+    // Direction = -1 === Up
+    // Direction = 1 === Down
+    if (currentStep + direction < 0) {
+      return;
+    } else if (currentStep + direction > steps.length - 1) {
+      return;
+    } else {
+      currentStep = currentStep + direction;
+    }
+
+    if (direction === 1) {
+      steps = [
+        ...steps.map((step: Step) => ({
+          ...step,
+          index: step.index - 1,
+        })),
+      ];
+    } else {
+      steps = [
+        ...steps.map((step: Step) => ({
+          ...step,
+          index: step.index + 1,
+        })),
+      ];
+    }
+  }
+
+  $: console.log(steps.map((step: Step) => step.index));
+
+  function mouseWheel(e) {
+    var direction = e.deltaY > 0 ? 1 : -1;
+    moveSections(direction);
+  }
+
+  window.addEventListener("mousemove", (e) => {
+    if (window.innerWidth < 640) {
+      return;
+    }
+
+    var obg: HTMLDivElement = document.querySelector(".object");
+    var cur: HTMLDivElement = document.querySelector(".cursor");
+    cur.style.left = e.pageX + "px";
+    cur.style.top = e.pageY + "px";
+    setTimeout(() => {
+      obg.style.left = e.pageX + "px";
+      obg.style.top = e.pageY + "px";
+    }, 50);
+  });
+
+  window.addEventListener(
+    "mousewheel",
+    debounce((e) => mouseWheel(e), 300)
+  );
+
+  window.addEventListener("keydown", (e) => {
+    e.key === "ArrowDown" && debounce(() => moveSections(1), 300)();
+    e.key === "ArrowUp" && debounce(() => moveSections(-1), 300)();
+  });
+
+  let steps: Step[] = STEPS;
+
+  function goTo(index: number) {
+    console.log("New index: ", index);
+    currentStep = index;
+    steps = STEPS.map((step: Step) => ({
+      ...step,
+      index: step.index - index,
+    }));
+  }
+</script>
+
+<main>
+  <Header {goTo} />
+  <div class="indicator">
+    <div class="line" />
+    <p class="title">
+      {steps[currentStep].stepTitle}
+    </p>
+  </div>
+  <div class="step">
+    {steps[currentStep].step} / 4
+  </div>
+  <div class="object" />
+  <div class="cursor" />
+  <div class="sections">
+    {#each steps as section}
+      {#if !!section.component}
+        <svelte:component
+          this={section.component}
+          last={section.index === -1}
+          index={section.index}
+        />
+      {:else}
+        <Section
+          src={section.src}
+          alt={section.alt}
+          title={section.title}
+          center={section.index === 0}
+          last={section.index === -1}
+          index={section.index}
+        />
+      {/if}
+    {/each}
+  </div>
+</main>
+
+<style>
+  main {
+    text-align: center;
+    padding: 0.5rem;
+    margin: 0 auto;
+  }
+
+  .indicator {
+    display: none;
+    position: fixed;
+    transform: rotate(-90deg);
+    min-width: 200px;
+    justify-content: space-between;
+    flex-direction: row;
+    align-items: center;
+    flex-wrap: nowrap;
+    bottom: 6rem;
+    left: -4rem;
+  }
+
+  .indicator .line {
+    display: none;
+    flex: 1;
+    height: 1px;
+    margin-right: 0.5rem;
+    background: #ffffff85;
+  }
+
+  .indicator .title {
+    display: none;
+    font-size: 1.3rem;
+    font-weight: bold;
+    color: #ffffff85;
+  }
+
+  .step {
+    display: none;
+    position: fixed;
+    top: 50vh;
+    left: 0.8rem;
+    font-size: 1.5rem;
+    font-weight: bold;
+    color: #ffffff85;
+  }
+
+  .sections {
+    display: flex;
+    align-items: center;
+    flex-wrap: wrap;
+    width: 100%;
+    height: 100%;
+    flex-direction: column;
+  }
+
+  @media (min-width: 640px) {
+    .indicator {
+      left: -3rem;
+      display: flex;
+    }
+
+    .step {
+      left: 2rem;
+      display: block;
+    }
+
+    .indicator .line {
+      display: block;
+    }
+
+    .indicator .title {
+      display: block;
+    }
+  }
+
+  @media (min-width: 834px) {
+    .indicator {
+      left: -3rem;
+      display: flex;
+    }
+
+    .step {
+      left: 2rem;
+      display: block;
+    }
+  }
+
+  .object {
+    position: absolute;
+    pointer-events: none;
+    z-index: 10;
+    top: -100px;
+    left: -100px;
+    transform: translate(-50%, -50%);
+    width: 20px;
+    height: 20px;
+    border-radius: 50%;
+    background: rgb(131, 5, 143);
+    border: 1px solid rgb(131, 5, 143);
+  }
+
+  .cursor {
+    position: absolute;
+    pointer-events: none;
+    z-index: 10;
+    top: -100px;
+    left: -100px;
+    transform: translate(-50%, -50%);
+    width: 8px;
+    height: 8px;
+    border-radius: 50%;
+    background: lightgray;
+  }
+</style>
---
+++ src/components/Header.svelte
@@ -0,0 +1,86 @@
+<script lang="ts">
+  export let goTo: Function;
+  let menus = [
+    {
+      name: "Home",
+      index: 0,
+    },
+    {
+      name: "Sobre",
+      index: 1,
+    },
+    {
+      name: "Portfolio",
+      index: 2,
+    },
+    {
+      name: "Contact",
+      index: 5,
+    },
+  ];
+
+  function beforeGoTo(index: number) {
+    goTo(index);
+  }
+</script>
+
+<nav>
+  <ul>
+    {#each menus as menu}
+      <li>
+        <button on:click={() => beforeGoTo(menu.index)}>
+          <p>{menu.name}</p>
+        </button>
+      </li>
+    {/each}
+  </ul>
+</nav>
+
+<!-- on:mouseenter={handleMouseEnter}
+on:mouseleave={handleMouseLeave} -->
+<style>
+  nav {
+    z-index: 2;
+    width: 100%;
+    position: fixed;
+    top: 0;
+    left: 0;
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-end;
+    align-items: center;
+  }
+
+  ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+  }
+
+  li {
+    float: left;
+    margin: 16px;
+    height: 70px;
+  }
+
+  button {
+    all: unset;
+    border: none;
+    background: none;
+    color: #ffffff80;
+  }
+
+  button:hover,
+  button:focus,
+  button:active {
+    all: unset;
+    outline: none;
+  }
+
+  @media (min-width: 640px) {
+  }
+
+  @media (min-width: 834px) {
+  }
+</style>
